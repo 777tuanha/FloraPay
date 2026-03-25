e7ab9953b50eab0312c4d6ae17fdb0db364c5184
Ken Ha <777tuanha@gmail.com>
2026-03-25 15:42:57 +0700

    Initial Commit

---
+++ FloraPay/FloraPayApp.swift
@@ -0,0 +1,17 @@
+//
+//  FloraPayApp.swift
+//  FloraPay
+//
+//  Created by admin on 3/25/26.
+//
+
+import SwiftUI
+
+@main
+struct FloraPayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
